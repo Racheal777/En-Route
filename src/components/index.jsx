2cade44c018bd9427900834e1615381fc148f450
Racheal Kuranchie <kuranchieracheal@gmail.com>
2022-05-07 14:56:01 +0000

    testing

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import Navbars from './navbar'
 import Footer from './footer'
 import About from './about'
+import HomePage from './Homepage'
+import Example from './example'
 
 export default function Index() {
   return (
     <div>
         <Navbars/>
+      <Example/>
+      <HomePage/>
         <About/>
-        {/* <Footer/> */}
+      
     </div>
   )
 }
